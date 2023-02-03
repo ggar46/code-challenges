f5b45331f6f842f38b4e9ae5dff5bf352648b86a
Gisselle Garcia <gissellegar46@gmail.com>
2023-02-02 17:21:04 -0800

    Completed exercise 3

--- 4__js-fundamentals/3.js
+++ 4__js-fundamentals/3.js
@@ -6,18 +6,25 @@
 //   - Add an array to the beginning of the array. (ex: [["a","b","c"],1,2,3,4])
 //   - Return the first item from the first item in the array. (ex: "a")
 
-
+//Create an array of numbers, (ex: [1, 2, 3])
 let arrOfNum = [1,2,3];
+
 //first item in the array (ex: 1)
- 
+let firstElement = arrOfNum[0];
+console.log(firstElement);
 
 //last item in the array (ex: 3)
-
+let lastElement = arrOfNum[arrOfNum.length-1];
+console.log(lastElement);
 
 //new item to the end of the array (ex: [1,2,3,4])
-
+arrOfNum.push(4);
+console.log(arrOfNum);
 
 //add an array to the beginning of the array. (ex: [["a","b","c"],1,2,3,4])
-
+arrOfNum.unshift(["a","b","c"]);
+console.log(arrOfNum);
 
 //return the first item from the first item in the array. (ex: "a")
+let firstElementFromNewArr = arrOfNum[0][0];
+console.log(firstElementFromNewArr);
